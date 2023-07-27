cb992c5b0c9acade6016bc653158cae9da944d72
tomprogramuje <tom.severa@outlook.cz>
2023-07-27 08:54:19 +0200

    Update views.py

--- noteapp/notes/views.py
+++ noteapp/notes/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from .models import Note
 from .forms import NoteForm
 
